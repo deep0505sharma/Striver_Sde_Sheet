bdac1be62dbe6ccd10ab8233cb240c6b3f495e47
Deepak Sharma <83660142+deep0505sharma@users.noreply.github.com>
2022-08-22 20:22:57 +0530

    Add files via upload

---
+++ Bipartite_BFS.cpp
@@ -0,0 +1,29 @@
+bool partite(int src,vector<int>adj[],int color[]){
+    queue<int>q;
+    q.push(src);
+    color[src]=1;
+    while(!q.empty()){
+        int node=q.front();
+        q.pop();
+        for(auto it:adj[node]){
+            if(color[it]==-1){
+                color[it]=1-color[node];
+                q.push(it);
+            }
+            else if(color[it]==color[node])return false;
+        }
+    }
+    return true;
+}
+	bool isBipartite(int V, vector<int>adj[]){
+	    // Code here
+	    int color[V];
+	    memset(color,-1,sizeof(color));
+	    for(int i=0;i<V;i++){
+	        if(color[i]==-1){
+	            if(!partite(i,adj,color))return false;
+	        }
+	    }
+	    return true;
+	}
+
